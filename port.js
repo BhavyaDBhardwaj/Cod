45fedb312d8f724b0235c29880caeaa65e6b1a6c
BhavyaDBhardwaj <121045595+BhavyaDBhardwaj@users.noreply.github.com>
2023-08-16 15:58:20 +0530

    Update port.js

--- port.js
+++ port.js
@@ -2,16 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const downloadButton = document.getElementById('resume');
 
     downloadButton.addEventListener('click', function() {
-        // Replace 'example.pdf' with the actual path to your PDF file
-        const pdfPath = 'Portfolio/CV/CV_bhavya.pdf';
 
-        // Create an anchor element
+        const pdfPath = 'C:/Users/msi1/Documents/Portfolio/CV_bhavya.pdf';
+
         const anchor = document.createElement('a');
         anchor.href = pdfPath;
-        anchor.download = 'downloaded_CV.pdf'; // Set the desired file name for download
-        anchor.target = '_blank'; // Open the link in a new tab (optional)
+        anchor.download = 'downloaded_CV.pdf';
+        anchor.target = '_blank';
 
-        // Simulate a click on the anchor element
         anchor.click();
     });
 });
+
